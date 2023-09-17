e4e28e27048c4ac99c148a094843e966407532e2
nadeemnagarji <129616695+nadeemnagarji@users.noreply.github.com>
2023-09-17 19:30:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function writeOnDOMLoad(){
-	let textNode = document.createTextNode("DOM load sucess")
+	let textNode = document.createTextNode("DOM load success")
 	var body = document.body;       
 	body.appendChild(textNode)
 }
